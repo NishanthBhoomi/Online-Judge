21a0826b030b4c7ce26c7c81b8c369711c0aeebf
Nishanth Bhoomi <nishbhoomi1234@gmail.com>
2024-07-22 02:07:53 +0530

    final commits

--- backend/index.js
+++ backend/index.js
@@ -22,17 +22,16 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
+app.use('/public', express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set("view engine","ejs");
-app.use(express.static(path.join(__dirname, 'views')));
-app.use(express.static('public'));
 
 DBConnection();
 
 app.use('/',router);
 
 app.get("/",(req,res)=>{
-    res.send("Welcome");
+    res.render('index', { email: 'example@example.com', status: 'not verified' });
 });
 
 app.listen(8000,()=>{
